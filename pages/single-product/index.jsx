57da4e17f4244ce3bc5c9c2dad0c05f69d82c865
rahmanshifur <rahmanshifur235@gmail.com>
2022-12-01 22:43:49 +0600

    change file and some to plm solve

--- pages/single-product/index.jsx
+++ pages/single-product/index.jsx
@@ -1,11 +1,18 @@
 import ProductContent from "../../components/product-content"
 import RelatedProduct from "../../components/related-product"
-import ShopLink from "../shop/shop-link"
+import ShopLink from './../../components/shop-link/index';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
 
 const Product = () => {
     return (
         <div className="product ">
-            <ShopLink />
+            <ShopLink
+                mainLink={`HOME`}
+                subLink={`SHOP`}
+                icon={faArrowRight}
+                title={`Flexible WareLess Head Phone`}
+            />
             <div className="container">
                 <ProductContent />
                 <RelatedProduct />

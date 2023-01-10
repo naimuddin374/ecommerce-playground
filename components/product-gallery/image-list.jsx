924f9fbcde748456832f8ec2b8528da129edfe0c
Ridoy Hossain <mdridoyhossain24@gmail.com>
2023-01-11 00:44:59 +0600

    details img solve

--- components/product-gallery/image-list.jsx
+++ components/product-gallery/image-list.jsx
@@ -5,20 +5,20 @@ import singlePdtImg3 from "../../assets/img/poster-03 1.png"
 import singlePdtImg from "../../assets/img/shop/product-list/product-38.png"
 
 
-const ImageList = () => {
+const ImageList = ({changHandler}) => {
     return (
         <div>
             <Link href="#">
-                <Image className="sub- w-75 h-auto  p-3" src={singlePdtImg} />
+                <Image onClick={()=>changHandler(singlePdtImg)} className="sub- w-75 h-auto  p-3" src={singlePdtImg} />
             </Link>
             <Link href="#">
-                <Image className="sub- w-75 h-auto  p-3" src={singlePdtImg2} />
+                <Image onClick={()=>changHandler(singlePdtImg2)} className="sub- w-75 h-auto  p-3" src={singlePdtImg2} />
             </Link>
             <Link href="#">
-                <Image className="sub- w-75 h-auto  p-3" src={singlePdtImg} />
+                <Image onClick={()=>changHandler(singlePdtImg)} className="sub- w-75 h-auto  p-3" src={singlePdtImg} />
             </Link>
             <Link href="#">
-                <Image className="sub- w-75 h-auto  p-3" src={singlePdtImg3} />
+                <Image onClick={()=>changHandler(singlePdtImg3)} className="sub- w-75 h-auto  p-3" src={singlePdtImg3} />
             </Link>
         </div>
     )
